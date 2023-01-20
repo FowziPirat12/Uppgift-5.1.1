a8c7f5dd142dcbac781fe587121fa0923c0e876c
leonkarlsson1 <bang03001@utb.vaxjo.se>
2023-01-20 10:22:51 +0100

    Add project files.

---
+++ Uppgift 5.1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System; 
+namespace Uppgitfr_5._1
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            string[] mening = new string[3];
+            for (int i = 0; i < 3; i++)
+            {
+               Console.WriteLine("skriv ner några meningar:" + mening[i]);
+               mening[i] = Console.ReadLine();
+               Console.WriteLine();
+                
+            }
+        }
+    }
+}
